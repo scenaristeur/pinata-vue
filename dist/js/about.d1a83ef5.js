e5ea85abcc0225add276b7d9acb400d5596666dd
Scenaristeur <scenaristeur@gmail.com>
2022-06-03 17:07:21 +0200

    Initial dist subtree commit

---
+++ dist/js/about.d1a83ef5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpinata_vue"]=self["webpackChunkpinata_vue"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),l={},r=(0,s.Z)(l,u,a,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.d1a83ef5.js.map
